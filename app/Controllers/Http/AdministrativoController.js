58dcbb49d8be34d25f5314d5ae85700bf7450ca5
0zz1cl13 <seniorcl13@gmail.com>
2020-01-13 13:49:57 -0400

    feat(administrativo):adicionado a criaçao de usuario administrativo

---
+++ app/Controllers/Http/AdministrativoController.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const Administrativo = use('App/Models/Administrativo')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with administrativos
+ */
+class AdministrativoController {
+  /**
+   * Show a list of all administrativos.
+   * GET administrativos
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new administrativo.
+   * GET administrativos/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+    const data = request.all()
+
+    const administrativo = await Administrativo.create(data)
+
+    return administrativo
+  }
+
+  /**
+   * Display a single administrativo.
+   * GET administrativos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update administrativo details.
+   * PUT or PATCH administrativos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a administrativo with id.
+   * DELETE administrativos/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = AdministrativoController
---
+++ database/migrations/1578936362691_administrativo_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AdministrativoSchema extends Schema {
+  up () {
+    this.create('administrativos', (table) => {
+      table.increments()
+      table.string('nome', 254).notNullable()
+      table.string('cpf', 11).notNullable().unique()
+      table.string('email', 254).notNullable().unique()
+      table.string('senha', 60).notNullable()
+      table.string('contato', 16).notNullable()
+      table.string('unidade', 254).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('administrativos')
+  }
+}
+
+module.exports = AdministrativoSchema
